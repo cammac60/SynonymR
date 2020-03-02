389ead67c0317cdd01f43abddd263251b9f75971
Cameron MacRae <cam.mac.cm@gmail.com>
2020-03-01 19:30:21 -0700

    Add form

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,17 @@
 <script>
-	export let name;
+	document.title = 'SynonymR';
+	let error;
+	let curWord;
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>SynonymR</h1>
+	<div class="form">
+		<input type="text" placeholder="Enter a word" bind:value={curWord}>
+		<button>Find Synonym</button>
+	</div>
+	<p class="error">{error || ''}</p>
 </main>
 
 <style>
@@ -16,18 +24,39 @@
 	}
 
 	main {
-		background: rgb(29,98,158);
-		background: radial-gradient(circle, rgba(29,98,158,1) 0%, rgba(0,0,0,1) 100%);
+		background: rgb(0,67,68);
+		background: linear-gradient(0deg, rgba(0,67,68,1) 0%, rgba(0,0,0,1) 100%);
 		height: 100vh;
+		padding-top: 5%;
 		width: 100%;
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
+		color: #FFFFFF;
+		font-size: 6em;
 		font-weight: 100;
 		margin: 0;
+	}
+
+	.form {
+		display: flex;
+		justify-content: space-between;
+		margin-left: auto;
+		margin-right: auto;
+		margin-top: 5%;
+		width: 30%;
+	}
+
+	input {
+		width: 60%;
+	}
+
+	.error {
+		color: red;
+		margin-left: auto;
+		margin-right: auto;
+		text-align: left;
+		width: 30%;
 	}
 
 </style>
