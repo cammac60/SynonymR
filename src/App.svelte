bff30b8735df74e4790b55c97222d3fb9eb32efa
Cameron MacRae <cam.mac.cm@gmail.com>
2020-03-01 20:49:59 -0700

    Finish main styles

--- src/App.svelte
+++ src/App.svelte
@@ -2,16 +2,19 @@
 	document.title = 'SynonymR';
 	let error;
 	let curWord;
-
+	let foundWords = [];
 </script>
 
 <main>
 	<h1>SynonymR</h1>
 	<div class="form">
 		<input type="text" placeholder="Enter a word" bind:value={curWord}>
-		<button>Find Synonym</button>
+		<button>Find Synonyms</button>
 	</div>
 	<p class="error">{error || ''}</p>
+	<div class="word-wrapper">
+
+	</div>
 </main>
 
 <style>
@@ -27,6 +30,7 @@
 		background: rgb(0,67,68);
 		background: linear-gradient(0deg, rgba(0,67,68,1) 0%, rgba(0,0,0,1) 100%);
 		height: 100vh;
+		overflow-x: scroll;
 		padding-top: 5%;
 		width: 100%;
 	}
@@ -60,9 +64,27 @@
 		width: 30%;
 	}
 
+	button {
+		cursor: pointer;
+	}
+
 	button:active {
 		color: #FFFFFF;
 		background: rgba(0,67,68,1);
+	}
+
+	.word-wrapper {
+		color: #FFFFFF;
+		display: grid;
+		font-size: 2rem;
+		grid-template-columns: repeat(4, 1fr);
+		grid-column-gap: 20px;
+		grid-row-gap: 20px;
+		margin-left: auto;
+		margin-right: auto;
+		margin-top: 5%;
+		text-align: center;
+		width: 60%;
 	}
 
 </style>
